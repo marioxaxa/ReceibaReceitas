ae46fea9ecd42e27c168f3facbe4db4cca76d84f
marcosnoberto <marcos.noberto.014@gmail.com>
2024-07-20 11:48:50 -0300

    Alterações

--- login/script/firebase.js
+++ login/script/firebase.js
@@ -1,64 +1,69 @@
-// Import the functions you need from the SDKs you need
+// Importar as funções necessárias dos SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, setDoc, addDoc, doc, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAqfNOpVNGAzorp5_0tuaI1QDd0WDZXqSo",
   authDomain: "receibareceitas.firebaseapp.com",
   projectId: "receibareceitas",
   storageBucket: "receibareceitas.appspot.com",
   messagingSenderId: "21963335608",
-  appId: "1:21963335608:web:6ad28cb59e8e253b13d14d",
-  measurementId: "G-KQKYLWV1Z9"
+  appId: "1:21963335608:web:7437d657904cda9c13d14d",
+  measurementId: "G-TTF55ZV2K0"
 };
 
-// Initialize Firebase
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const auth = getAuth(app);
 
+// Adicionar evento para o botão de registro
+document.getElementById('btnRegistrar').addEventListener('click', async (e) => {
+  e.preventDefault();
+  const login = document.getElementById('loginRegistro').value;
+  const email = document.getElementById('emailRegistro').value;
+  const senha = document.getElementById('senha2').value;
 
-btnRegistrar.addEventListener('click', (e)=>{
-    var login = document.getElementById('loginRegistro').value;
-    var email = document.getElementById('emailRegistro').value;
-    var senha = document.getElementById('senha2').value;
-
-    addDoc(collection(db, "usuarios"), {
-        email: email,
-        img:"",
-        usuario: login,
-        senha: senha
-      });
-    
-    alert("Usuario adicionado.")
+  try {
+    await addDoc(collection(db, "usuarios"), {
+      email: email,
+      img: "",
+      usuario: login,
+      senha: senha
+    });
+    alert("Usuário adicionado.");
+  } catch (error) {
+    console.error("Erro ao adicionar usuário: ", error);
+  }
 });
 
-btnLogar.addEventListener('click', (e)=>{
-    var email = document.getElementById('email').value;
-    var senha = document.getElementById('senha1').value;
-    var naoCadastrado = true;
+// Adicionar evento para o botão de login
+document.getElementById('btnLogar').addEventListener('click', async (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha1').value;
+  let naoCadastrado = true;
 
-    getDocs(collection(db, "usuarios")).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            if (email == doc.data().email) {
-                if(senha == doc.data().senha){
-                    alert('Logado com sucesso')
-                    window.location.href = "../p_perfil/p_perfil.html";
-                }else{
-                    alert('Senha diferente')
-                }
-                naoCadastrado = false;
-            }
-        });
-
-        if (naoCadastrado) {
-            alert('Usuario não encontrado')
+  try {
+    const querySnapshot = await getDocs(collection(db, "usuarios"));
+    querySnapshot.forEach((doc) => {
+      if (email === doc.data().email) {
+        if (senha === doc.data().senha) {
+          alert('Logado com sucesso');
+          window.location.href = "../p_perfil/p_perfil.html";
+        } else {
+          alert('Senha diferente');
         }
-    }).catch((error) => {
-        console.error("Erro ao obter documentos: ", error);
+        naoCadastrado = false;
+      }
     });
+
+    if (naoCadastrado) {
+      alert('Usuário não encontrado');
+    }
+  } catch (error) {
+    console.error("Erro ao obter documentos: ", error);
+  }
 });
