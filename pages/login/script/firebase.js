760a69c5bb76599eab54c237749f2483b7787e3a
marioxaxa <marioxaxa0@gmail.com>
2024-07-22 22:02:21 -0300

    fix: adicionado usuario no cookie

--- pages/login/script/firebase.js
+++ pages/login/script/firebase.js
@@ -1,17 +1,26 @@
 // Importar as funções necessárias dos SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    where,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import setCookie from "../../../src/utils/setCookie";
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAqfNOpVNGAzorp5_0tuaI1QDd0WDZXqSo",
-  authDomain: "receibareceitas.firebaseapp.com",
-  projectId: "receibareceitas",
-  storageBucket: "receibareceitas.appspot.com",
-  messagingSenderId: "21963335608",
-  appId: "1:21963335608:web:7437d657904cda9c13d14d",
-  measurementId: "G-TTF55ZV2K0"
+    apiKey: "AIzaSyAqfNOpVNGAzorp5_0tuaI1QDd0WDZXqSo",
+    authDomain: "receibareceitas.firebaseapp.com",
+    projectId: "receibareceitas",
+    storageBucket: "receibareceitas.appspot.com",
+    messagingSenderId: "21963335608",
+    appId: "1:21963335608:web:7437d657904cda9c13d14d",
+    measurementId: "G-TTF55ZV2K0",
 };
 
 // Inicializar o Firebase
@@ -21,68 +30,75 @@ const auth = getAuth(app);
 
 // Função para verificar se e-mail ou nome de usuário já estão em uso
 async function isUserAlreadyExists(email, usuario) {
-  const emailQuery = query(collection(db, "usuarios"), where("email", "==", email));
-  const usuarioQuery = query(collection(db, "usuarios"), where("usuario", "==", usuario));
+    const emailQuery = query(
+        collection(db, "usuarios"),
+        where("email", "==", email)
+    );
+    const usuarioQuery = query(
+        collection(db, "usuarios"),
+        where("usuario", "==", usuario)
+    );
 
-  const [emailSnapshot, usuarioSnapshot] = await Promise.all([
-    getDocs(emailQuery),
-    getDocs(usuarioQuery)
-  ]);
+    const [emailSnapshot, usuarioSnapshot] = await Promise.all([
+        getDocs(emailQuery),
+        getDocs(usuarioQuery),
+    ]);
 
-  return !emailSnapshot.empty || !usuarioSnapshot.empty;
+    return !emailSnapshot.empty || !usuarioSnapshot.empty;
 }
 
 // Adicionar evento para o botão de registro
-document.getElementById('btnRegistrar').addEventListener('click', async (e) => {
-  e.preventDefault();
-  const login = document.getElementById('loginRegistro').value;
-  const email = document.getElementById('emailRegistro').value;
-  const senha = document.getElementById('senha2').value;
+document.getElementById("btnRegistrar").addEventListener("click", async (e) => {
+    e.preventDefault();
+    const login = document.getElementById("loginRegistro").value;
+    const email = document.getElementById("emailRegistro").value;
+    const senha = document.getElementById("senha2").value;
 
-  // Verificar se o e-mail ou nome de usuário já estão em uso
-  if (await isUserAlreadyExists(email, login)) {
-    alert('E-mail ou nome de usuário já está em uso.');
-    return;
-  }
+    // Verificar se o e-mail ou nome de usuário já estão em uso
+    if (await isUserAlreadyExists(email, login)) {
+        alert("E-mail ou nome de usuário já está em uso.");
+        return;
+    }
 
-  try {
-    await addDoc(collection(db, "usuarios"), {
-      email: email,
-      img: "",
-      usuario: login,
-      senha: senha
-    });
-    alert("Usuário adicionado.");
-  } catch (error) {
-    console.error("Erro ao adicionar usuário: ", error);
-  }
+    try {
+        await addDoc(collection(db, "usuarios"), {
+            email: email,
+            img: "",
+            usuario: login,
+            senha: senha,
+        });
+        alert("Usuário adicionado.");
+    } catch (error) {
+        console.error("Erro ao adicionar usuário: ", error);
+    }
 });
 
 // Adicionar evento para o botão de login
-document.getElementById('btnLogar').addEventListener('click', async (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const senha = document.getElementById('senha1').value;
-  let naoCadastrado = true;
+document.getElementById("btnLogar").addEventListener("click", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha1").value;
+    let naoCadastrado = true;
 
-  try {
-    const querySnapshot = await getDocs(collection(db, "usuarios"));
-    querySnapshot.forEach((doc) => {
-      if (email === doc.data().email) {
-        if (senha === doc.data().senha) {
-          alert('Logado com sucesso');
-          window.location.href = "../p_perfil/p_perfil.html";
-        } else {
-          alert('Senha diferente');
+    try {
+        const querySnapshot = await getDocs(collection(db, "usuarios"));
+        querySnapshot.forEach((doc) => {
+            if (email === doc.data().email) {
+                if (senha === doc.data().senha) {
+                    setCookie('usuario', doc.data().usuario, 1);
+                    alert("Logado com sucesso");
+                    window.location.href = "../p_perfil/p_perfil.html";
+                } else {
+                    alert("Senha diferente");
+                }
+                naoCadastrado = false;
+            }
+        });
+
+        if (naoCadastrado) {
+            alert("Usuário não encontrado");
         }
-        naoCadastrado = false;
-      }
-    });
-
-    if (naoCadastrado) {
-      alert('Usuário não encontrado');
+    } catch (error) {
+        console.error("Erro ao obter documentos: ", error);
     }
-  } catch (error) {
-    console.error("Erro ao obter documentos: ", error);
-  }
 });
