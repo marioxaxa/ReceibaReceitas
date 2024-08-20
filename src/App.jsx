1a4668ae479c5b4c851dd94f6d4848a028cc0dbb
marcosnoberto <marcos.noberto.014@gmail.com>
2024-08-20 15:01:41 -0300

    Pagina Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import Inicial from "./pages/Inicial/Inicial";
 import PDeReceita from "./pages/PDeReceita/PDeReceita";
 import Login from './pages/Login/login';
 import Ranking from "./pages/Ranking/ranking.jsx";
+import Sobre from './pages/Sobre/Sobre.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <>
             <ThemeContext>
-                <Ranking />
+                <Sobre />
             </ThemeContext>
         </>
     );
